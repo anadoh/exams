eda9dc414137297f459124a7abc1f0dc9c7ea0d7
anadoh <68846949+anadoh@users.noreply.github.com>
2021-12-13 17:37:42 +0100

    table filtering

--- src/app/exams/exams.module.ts
+++ src/app/exams/exams.module.ts
@@ -8,6 +8,7 @@ import { MatInputModule } from '@angular/material/input';
 
 
 
+
 @NgModule({
   declarations: [
     ExamsComponent,
@@ -16,7 +17,8 @@ import { MatInputModule } from '@angular/material/input';
   imports: [
     CommonModule,
     SharedModule,
-    MatInputModule
+    MatInputModule,
+
     
   ],
   exports: [ExamsComponent]
--- src/app/exams/schedule/schedule.component.ts
+++ src/app/exams/schedule/schedule.component.ts
@@ -1,42 +1,100 @@
-
 import { Component } from '@angular/core';
-
+import { MatTableDataSource } from '@angular/material/table';
 
 export interface PeriodicElement {
   name: string;
   position: number;
   data: string;
   type: string;
-  teachers: string
+  teachers: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-
-  {position: 1, name: 'IRPUB Global Economic Governance', data: '04/02/2022', type: 'oral', teachers: 'John Doe'},
-  {position: 2, name: 'IRPUB Global Economic Governance', data: '05/02/2022', type: 'written', teachers: 'Wiliam Rudolf'},
-  {position: 3, name: 'ISAD Migration and Development', data: '07/02/2022', type: 'oral', teachers: 'Alice Werk'},
-  {position: 4, name: 'ISAD Migration and Development', data: '08/02/2022', type: 'written', teachers: 'Alice Werk'},
-  {position: 5, name: 'IRPUB Global Economic', data: '06/02/2022', type: 'oral', teachers: 'Mark Spencer'},
-  {position: 6, name: 'IE International Economics,', data: '01/02/2022', type: 'oral', teachers: 'Ben Hur'},
-  {position: 7, name: 'IE International Economics,', data: '04/02/2022', type: 'written', teachers: 'Ben Hur'},
-  {position: 8, name: 'IRPUB Global Economic Governance', data: '14/02/2022', type: 'oral', teachers: 'Wiliam Rudolf'},
-  {position: 9, name: 'IRPUB Global Economic', data: '12/02/2022', type: 'written', teachers: 'Mark Spencer'}
-
+  {
+    position: 1,
+    name: 'IRPUB Global Economic Governance',
+    data: '04/02/2022',
+    type: 'oral',
+    teachers: 'John Doe',
+  },
+  {
+    position: 2,
+    name: 'IRPUB Global Economic Governance',
+    data: '05/02/2022',
+    type: 'written',
+    teachers: 'Wiliam Rudolf',
+  },
+  {
+    position: 3,
+    name: 'ISAD Migration and Development',
+    data: '07/02/2022',
+    type: 'oral',
+    teachers: 'Alice Werk',
+  },
+  {
+    position: 4,
+    name: 'ISAD Migration and Development',
+    data: '08/02/2022',
+    type: 'written',
+    teachers: 'Alice Werk',
+  },
+  {
+    position: 5,
+    name: 'IRPUB Global Economic',
+    data: '06/02/2022',
+    type: 'oral',
+    teachers: 'Mark Spencer',
+  },
+  {
+    position: 6,
+    name: 'IE International Economics,',
+    data: '01/02/2022',
+    type: 'oral',
+    teachers: 'Ben Hur',
+  },
+  {
+    position: 7,
+    name: 'IE International Economics,',
+    data: '04/02/2022',
+    type: 'written',
+    teachers: 'Ben Hur',
+  },
+  {
+    position: 8,
+    name: 'IRPUB Global Economic Governance',
+    data: '14/02/2022',
+    type: 'oral',
+    teachers: 'Wiliam Rudolf',
+  },
+  {
+    position: 9,
+    name: 'IRPUB Global Economic',
+    data: '12/02/2022',
+    type: 'written',
+    teachers: 'Mark Spencer',
+  },
 ];
-
 
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
-  styleUrls: ['./schedule.component.css']
+  styleUrls: ['./schedule.component.css'],
 })
 
-
-
 export class ScheduleComponent {
-  displayedColumns: string[] = ['position','name','data','type','teachers']
-  dataSource = ELEMENT_DATA;
-
+  displayedColumns: string[] = ['position', 'name', 'data', 'type', 'teachers'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+ 
+  constructor () {
+    this.dataSource.filterPredicate = function (data, filter: string): boolean {
+      return (
+        data.name.toLowerCase().includes(filter) ||
+        data.type.toLowerCase().includes(filter)
+      );
+    };
   }
 
-
+  applyFilter(event: any) {
+    this.dataSource.filter = event.target.value.trim().toLowerCase();
+  }
+}
